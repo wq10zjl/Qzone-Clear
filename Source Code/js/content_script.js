782d6c86309f6c77c5975c0be56557234788b5b0
Wu Daofeng <dolphin.w.e@gmail.com>
2014-01-15 15:36:37 +0800

    v0.1.1版更新
    添加了多关键字匹配

--- Source Code/js/content_script.js
+++ Source Code/js/content_script.js
@@ -9,6 +9,7 @@ var $j = jQuery.noConflict();
 
 function clearOut() {
     var content = [];
+    var multi = [];
     var ele = $j(".q_namecard,.f-like .item");
     if (localStorage.getItem("hidePart") !== null) {
         var target = localStorage.getItem("hidePart").split(",");
@@ -19,6 +20,9 @@ function clearOut() {
         if (temp !== null) {
             content.push(temp.join(""))
         } // 获得hidePart中文本部分
+        if (target[i].indexOf("+") > -1) {
+            multi.push(target[i]);
+        } // 获取多关键字部分
         var t = target[i];
         for (var j = 0; j < ele.length; j++) {
             if ($j(ele[j]).attr("href") !== undefined) {
@@ -54,6 +58,39 @@ function clearOut() {
             if (text.indexOf(content[k]) > -1) {
                 $j(this).remove(); // 为评论内容时移除评论内容
             }
+        })
+    };
+
+    // 多关键字匹配
+    if (multi.length !== 0) {
+        $j(multi).each(function(i) {
+            var arr = multi[i].split("+");
+            var ck;
+            $j(arr).each(function(e) {
+                var ele = $j(".f-info, .f-ct-txtimg, .f-user-info");
+                $j(ele).each(function() {
+                    var matchText = $j(this).text();
+                    if (matchText.indexOf(arr[0]) !== -1 && matchText.indexOf(arr[e]) === -1) {
+                        ck = 0;
+                        return false;
+                    } else if (matchText.indexOf(arr[0]) !== -1 && matchText.indexOf(arr[e]) !== -1) {
+                        ck = $j(this).parents(".f-single");
+                    }
+                }); // 不为评论内容时移除整体
+                var ctEle = $j(".comments-item");
+                $j(ctEle).each(function() {
+                    var matchText = $j(this).text();
+                    if (matchText.indexOf(arr[0]) !== -1 && matchText.indexOf(arr[e]) === -1) {
+                        ck = 0;
+                        return false;
+                    } else if (matchText.indexOf(arr[0]) !== -1 && matchText.indexOf(arr[e]) !== -1) {
+                        ck = $j(this);
+                    }
+                }); // 为评论内容时移除评论内容
+            })
+            if (ck !== undefined && ck !== 0) {
+                    $j(ck).remove()
+            };
         })
     };
 }
